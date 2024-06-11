1a5cd0d34a5225fd4545987583160e258c9e7e05
quantanmreaper <138207080+quantanmreaper@users.noreply.github.com>
2024-06-11 16:14:50 +0300

    Sign up and login modal boxes and other changes

--- js/script.js
+++ js/script.js
@@ -1,25 +1,39 @@
-// Get the modal
-var modal = document.getElementById("signup-modal");
+// Get the modal elements
+var signupModal = document.getElementById("signup-modal");
+var loginModal = document.getElementById("login-modal");
 
-// Get the link that opens the modal
-var btn = document.getElementById("signup-link");
+// Get the buttons that open the modals
+var signupBtn = document.getElementById("signup-link");
+var loginBtn = document.getElementById("login-into");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the <span> elements that close the modals
+var closeButtons = document.getElementsByClassName("close");
 
-// When the user clicks the link, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+// When the user clicks the signup button, open the signup modal
+signupBtn.onclick = function() {
+    signupModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+// When the user clicks the login link, open the login modal
+loginBtn.onclick = function() {
+    signupModal.style.display = "none"
+    loginModal.style.display = "block";
+}
+
+// Assign the close functionality to each close button
+closeButtons[0].onclick = function() {
+    signupModal.style.display = "none";
+}
+
+closeButtons[1].onclick = function() {
+    loginModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == signupModal) {
+        signupModal.style.display = "none";
+    } else if (event.target == loginModal) {
+        loginModal.style.display = "none";
     }
 }
